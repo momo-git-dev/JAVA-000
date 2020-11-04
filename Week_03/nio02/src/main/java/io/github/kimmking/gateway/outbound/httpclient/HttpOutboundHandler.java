9013bb506c970f11bcda979c891618d7ee7c6480
billwugit <billwugit@192.168.68.13>
2020-11-04 17:11:50 +0800

    整合httpclient

---
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/httpclient/HttpOutboundHandler.java
@@ -0,0 +1,73 @@
+package io.github.kimmking.gateway.outbound.httpclient;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpOutboundHandler {
+
+    private CloseableHttpClient httpClient;
+    private String outboundUrl;
+
+    public HttpOutboundHandler(String outboundUrl) {
+        this.outboundUrl = outboundUrl;
+        httpClient = HttpClients.createDefault();
+    }
+
+    public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx) {
+        final String url = this.outboundUrl + fullRequest.uri();
+        get(url, fullRequest, ctx);
+    }
+
+    public void get(String url, final FullHttpRequest fullRequest, final ChannelHandlerContext ctx) {
+        System.out.println("url: " + url);
+        System.out.println("outboundUrl: " + outboundUrl);
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setHeader("Connection", "keep-alive");
+        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
+
+        CloseableHttpResponse httpResponse = null;
+        try {
+            handleResponse(fullRequest, ctx, httpResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, final HttpResponse endpointResponse) {
+        FullHttpResponse response = null;
+        try {
+            String value = "hello,this is momo";
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+        }
+
+    }
+}
